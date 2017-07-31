2bbdb64e8e20c5aa83a1a3dfa66631035cb167e6
meateviler <meateviler@msn.com>
2017-07-31 15:44:26 +0800

    add StringAverage

---
+++ StringAverage/Kata.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StringAverage
+{
+	public class Kata
+	{
+		private static Dictionary<string, int> stringToInt = new Dictionary<string, int>()
+		{
+			{"zero",0},
+			{"one",1},
+			{"two",2},
+			{"three",3},
+			{"four",4},
+			{"five",5},
+			{"six",6},
+			{"seven",7},
+			{"eight",8},
+			{"nine",9},
+		};
+
+		public static string AverageString(string str)
+		{
+			if (str.Trim() == string.Empty)
+				return "n/a";
+			var numString = str.Split(' ');
+			var sum = 0;
+			foreach (var numStr in numString)
+			{
+				if (!stringToInt.ContainsKey(numStr))
+					return "n/a";
+			}
+			foreach (var numStr in numString)
+			{
+				sum += ParseInt(numStr.ToLower());
+			}
+
+			return ParseIntToString(sum / numString.Length);
+		}
+
+		private static string ParseIntToString(int p)
+		{
+			return stringToInt.FirstOrDefault(x => x.Value==p).Key;
+		}
+
+		private static int ParseInt(string numStr)
+		{
+			return stringToInt[numStr];
+		}
+	}
+}
---
+++ StringAverageTests/KataTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StringAverage;
+
+namespace StringAverage.Tests
+{
+	[TestFixture()]
+	public class KataTests
+	{
+		[TestCase("", ExpectedResult = "n/a", TestName = "Empty_Should_be_n/a")]
+		[TestCase("five four", ExpectedResult = "four", TestName = "five four_Should_be_four")]
+		[TestCase("one one eight one", ExpectedResult = "two", TestName = "one one eight one_Should_be_two")]
+		[TestCase("zero nine five two", ExpectedResult = "four", TestName = "zero nine five two_Should_be_four")]
+		[TestCase("four six two three", ExpectedResult = "three", TestName = "four six two three_Should_be_three")]
+		[TestCase("one two three four five", ExpectedResult = "three", TestName = "one two three four five_Should_be_three")]
+		[TestCase("zero zero zero zero zero", ExpectedResult = "zero", TestName = "zero zero zero zero zero five_Should_be_zero")]
+		[TestCase("four six nine tphunt two six", ExpectedResult = "n/a", TestName = "four six nine tphunt two six_Should_be_n/a")]
+		public string AverageStringTest(string input)
+		{
+			return Kata.AverageString(input);
+		}
+	}
+}
